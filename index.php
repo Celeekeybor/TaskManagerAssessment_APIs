54e5962b4269c7f7a6ba198e9cfe3fce35f06090
Clifford537 <cliffordmukosh@gmail.com>
2025-07-13 16:03:41 +0300

    updates

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // --- CORS Headers ---
-header("Access-Control-Allow-Origin: *"); // Allow all origins or specify exact domain
+header("Access-Control-Allow-Origin: *"); 
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
@@ -9,7 +9,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit;
 }
-
 
 
 require_once 'routes/api.php';
